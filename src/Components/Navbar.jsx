04060ca026e9fa52ded87b598a993d0f3dcb9bb6
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-03-04 22:43:18 +0530

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,41 +3,38 @@ import Api from "../Utils/api";
 import { authContext } from "../Context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/system";
-import {
-  Grid,
-  Typography,
-  Button,
-  AppBar,
-  Toolbar,
-  IconButton,
-} from "@mui/material";
+import { Typography, Button, AppBar, Toolbar, IconButton } from "@mui/material";
 
 const Navbar = () => {
   const ctx = useContext(authContext);
   const navigate = useNavigate();
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }} py={2}>
       <AppBar
         position="static"
-        sx={{ background: "transparent", boxShadow: "none" }}
+        sx={{
+          background: "#232323",
+          boxShadow: "none",
+          borderRadius: "20px",
+        }}
       >
         <Toolbar>
           <IconButton>
             <i
-              className="fa-regular fa-square fa-xl"
+              className="fa-regular fa-square fa-lg"
               style={{
                 color: "#01ab81",
               }}
             ></i>
           </IconButton>
           <Typography
-            variant="h5"
+            variant="h6"
             component="div"
             sx={{ flexGrow: 1, color: "white" }}
           >
             camelCase
           </Typography>
-          <Typography color={"White"} fontSize={"1.2rem"} mr={2}>
+          <Typography color={"White"} fontSize={"1.1rem"} mr={2}>
             {ctx.user.first_name} {ctx.user.last_name}
           </Typography>
           <Button
@@ -52,8 +49,9 @@ const Navbar = () => {
               backgroundColor: "#01ab81",
               border: "1px solid #01ab81",
               transition: "0.4s",
-              padding: "5px 10px",
+              padding: "4px 8px",
               color: "white",
+              fontSize: "0.8rem",
               "&:hover": {
                 background: "transparent",
                 color: "#01ab81",
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,74 +1,15 @@
-// import React, { useContext } from "react";
-// import Api from "../Utils/api";
-// import { authContext } from "../Context/AuthContext";
-// import { useNavigate } from "react-router-dom";
-// import { Box } from "@mui/system";
-// import { Grid, Typography, Button } from "@mui/material";
-// import SideBar from "../Components/HomePageComponents/SideBar";
-// import RightBar from "../Components/HomePageComponents/RightBar";
-// import Feed from "../Components/HomePageComponents/Feed";
-// import Navbar from "../Components/Navbar";
-
-// const Home = () => {
-//   const ctx = useContext(authContext);
-//   const navigate = useNavigate();
-//   return (
-//     <Box sx={{ backgroundColor: "#181818", minHeight: "100vh" }}>
-//       <Grid
-//         container
-//         justifyContent={"center"}
-//         alignItems={"center"}
-//         flexDirection={"column"}
-//         sx={{ minHeight: "100vh" }}
-//       >
-//         <Grid item lg={6}>
-//           <Typography color={"White"} fontSize={"1.4rem"}>
-//             <Box
-//               component={"span"}
-//               style={{ display: "inline", color: "#01ab81" }}
-//             >
-//               Welcome
-//             </Box>
-//             , {ctx.user.first_name} {ctx.user.last_name}
-//           </Typography>
-
-//           <Button
-//             sx={{
-//               display: "block",
-//               backgroundColor: "#01ab81",
-//               color: "white",
-//               my: 4,
-//               "&:hover": {
-//                 backgroundColor: "#007d5e",
-//               },
-//             }}
-//             fullWidth
-//             onClick={() => {
-//               Api.get("/auth/logout").then((res) => {
-//                 ctx.setUser(null);
-//                 navigate("/signin");
-//               });
-//             }}
-//           >
-//             Log out
-//           </Button>
-//         </Grid>
-//       </Grid>
-//     </Box>
-//   );
-// };
-
-// export default Home;
-
 import React from "react";
 import Navbar from "../Components/Navbar";
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 const Home = () => {
   return (
-    <Box sx={{ backgroundColor: "#181818", minHeight: "100vh" }}>
+    <Container
+      maxWidth={"xl"}
+      sx={{ backgroundColor: "#181818", minHeight: "100vh" }}
+    >
       <Navbar />
-    </Box>
+    </Container>
   );
 };
 
