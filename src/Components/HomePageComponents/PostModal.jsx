36004500521ae5d991b8c78ee70b0bb2c826f2ac
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-03-08 21:50:39 +0530

    changes

--- src/Components/HomePageComponents/PostModal.jsx
+++ src/Components/HomePageComponents/PostModal.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { Divider, Stack, TextField } from "@mui/material";
 
 const style = {
   position: "absolute",
@@ -13,11 +14,17 @@ const style = {
   bgcolor: "#232323",
   borderRadius: "25px",
   boxShadow: 24,
-  p: 4,
+  p: 2,
   color: "white",
+  border: "2px solid #333",
 };
 
 function PostModal({ open, handleClose }) {
+  const [text, setText] = useState("");
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
   return (
     <Modal
       open={open}
@@ -32,9 +39,62 @@ function PostModal({ open, handleClose }) {
           justifyContent={"center"}
           alignItems={"center"}
         >
-          <Typography id="modal-modal-title" sx={{ fontSize: "1.5rem" }}>
+          <Typography
+            id="modal-modal-title"
+            sx={{
+              fontSize: "1.5rem",
+              width: "100%",
+              textAlign: "center",
+            }}
+          >
             Create Post
+            <Divider
+              sx={{
+                maxWidth: "100%",
+                backgroundColor: "#444",
+                margin: "20px auto 0px auto",
+                color: "#fff",
+              }}
+            />
           </Typography>
+          <TextField
+            id="filled-basic"
+            label="What's on your mind..."
+            variant="filled"
+            fullWidth
+            autoComplete="off"
+            multiline
+            value={text}
+            onChange={handleTextChange}
+            sx={{
+              "& .MuiFilledInput-underline:after": {
+                borderBottomColor: "#01ab81",
+              },
+            }}
+            InputLabelProps={{
+              style: { color: "#888" },
+            }}
+            InputProps={{
+              style: { color: "white", background: "none" },
+              disableUnderline: true,
+            }}
+          />
+
+          <Button
+            fullWidth
+            disabled={!text}
+            sx={{
+              backgroundColor: text ? "#01ab81" : "gray",
+              color: text ? "#fff" : "#000",
+              padding: "10px",
+              borderRadius: "25px",
+              ":hover": {
+                backgroundColor: "#01ab81",
+              },
+            }}
+          >
+            Post
+          </Button>
         </Box>
       </Box>
     </Modal>
