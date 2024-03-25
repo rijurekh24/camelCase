5073782886acf0a98bb2c5ad4552c89147b1eeea
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-03-25 15:00:25 +0530

    dp upload func change

--- src/Components/Navbar/Search.jsx
+++ src/Components/Navbar/Search.jsx
@@ -16,19 +16,19 @@ const Search = () => {
   const [searchResult, setSearchResult] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
 
-  const handleSearch = (query) => {
-    setIsFetching(true);
-    Api.post("/auth/search", { query })
-      .then((response) => {
-        setSearchResult(response.data.search_res[0].users);
-      })
-      .catch((error) => {
-        console.error("Error:", error.response);
-      })
-      .finally(() => {
-        setIsFetching(false);
-      });
-  };
+  // const handleSearch = (query) => {
+  //   setIsFetching(true);
+  //   Api.post("/auth/search", { query })
+  //     .then((response) => {
+  //       setSearchResult(response.data.search_res[0].users);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error:", error.response);
+  //     })
+  //     .finally(() => {
+  //       setIsFetching(false);
+  //     });
+  // };
 
   const handleChange = (e) => {
     const { value } = e.target;
